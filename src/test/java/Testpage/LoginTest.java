507477e79c95e50c1d95258913d97aad1f30455a
mac <Thanuragayanaka9884@gmail.com>
2025-02-26 02:25:43 +0530

    Header Validation

--- src/test/java/Testpage/LoginTest.java
+++ src/test/java/Testpage/LoginTest.java
@@ -2,9 +2,10 @@ package Testpage;
 
 import Basepage.BasePage;
 import Pages.Loginpage;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
-import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
@@ -12,17 +13,16 @@ public class LoginTest extends BasePage {
 
     @BeforeMethod
     @Override
-    public void setup() {
+    public void setup() throws InterruptedException {
         super.setup();
     }
 
    // @AfterMethod
-    @Override
-    public void teardown() {
-        super.teardown();
-    }
+   @Override
+   public void teardown() {
+   }
 
-    @Test
+    @Test(priority = 0)
     public void ValidLogin () throws InterruptedException {
         Loginpage loginpage = new Loginpage(driver);  // driver will be inherited from BasePage
         loginpage.login("standard_user","secret_sauce");
@@ -30,4 +30,51 @@ public class LoginTest extends BasePage {
         String expectedUrl = "https://www.saucedemo.com/inventory.html";
         Assert.assertEquals(driver.getCurrentUrl(), expectedUrl, "Login failed!");
     }
+    @Test(priority = 2)
+    public void Without_any_Credectional() {
+        Loginpage loginpage = new Loginpage(driver);
+        loginpage.clickLogin();
+
+        // Validate error message
+        By errorLocator = By.xpath("//h3[@data-test='error']");  // Adjust the locator if needed
+        WebElement errorElement = driver.findElement(errorLocator);
+        String actualError = errorElement.getText();
+
+        String expectedError = "Epic sadface: Username is required";
+        Assert.assertEquals(actualError, expectedError, "Error message mismatch!");
+
+        // Ensure user remains on login page
+        Assert.assertNotEquals(driver.getCurrentUrl(), "https://www.saucedemo.com/inventory.html",
+                "User should not be logged in without credentials.");
+    }
+
+
+    @Test(priority = 3)
+    public void WithWrongPassword() throws InterruptedException {
+        Loginpage loginpage = new Loginpage(driver);
+        loginpage.login("test","dfwsdwsdw");
+
+        String expectedUrl = "https://www.saucedemo.com/inventory.html";
+        Assert.assertNotEquals(driver.getCurrentUrl(), expectedUrl, "Epic sadface: Username and password do not match any user in this service\n");
+
+
+    }
+
+    @Test(priority = 4)
+    public void LoginWithLockedOutUser() throws InterruptedException {
+        Loginpage loginpage = new Loginpage(driver);
+        loginpage.login("locked_out_user", "secret_sauce");
+
+        // Verify error message
+//       WebElement errorElement = driver.findElement(errorLocator);
+      //  String actualError = errorElement.getText();
+
+        String expectedError = "Epic sadface: Sorry, this user has been locked out.\n";
+       // Assert.assertEquals(actualError, expectedError, "Error message mismatch for locked out user.");
+    }
+
+
+
 }
+
+
